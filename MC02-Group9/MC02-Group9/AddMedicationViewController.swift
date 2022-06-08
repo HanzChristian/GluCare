9cb9ad3d64c686eecf32e90812492b5d4f13e5fb
HanzChristian <hanz.christian@ui.ac.id>
2022-06-08 14:35:59 +0700

    Fixed button and Add medication view gggemink

---
+++ MC02-Group9/MC02-Group9/AddMedicationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddMedicationViewController.swift
+//  MC02-Group9
+//
+//  Created by Hanz Christian on 08/06/22.
+//
+
+import UIKit
+
+class AddMedicationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Add Medication"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
