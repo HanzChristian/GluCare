ae78ee179563261203b27a7fc53b0619c7039e3d
HanzChristian <hanz.christian@ui.ac.id>
2022-06-09 14:57:19 +0700

    TableView cell with RowAction

---
+++ MC02-Group9/MC02-Group9/ViewControllerTVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerTVC.swift
+//  MC02-Group9
+//
+//  Created by Hanz Christian on 08/06/22.
+//
+
+import UIKit
+
+class ViewControllerTVC: UITableViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var cellImgView: UIImageView!
+    @IBOutlet weak var medLbl: UILabel!
+    @IBOutlet weak var freqLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
