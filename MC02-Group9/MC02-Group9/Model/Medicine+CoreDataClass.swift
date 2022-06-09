fd749b808c5faf59f759ba4b04dc0ebea4203057
davin <christophorusdavin@gmail.com>
2022-06-09 16:48:33 +0700

    core data 2

---
+++ MC02-Group9/MC02-Group9/Model/Medicine+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Medicine+CoreDataClass.swift
+//  
+//
+//  Created by Christophorus Davin on 09/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Medicine)
+public class Medicine: NSManagedObject {
+
+}
