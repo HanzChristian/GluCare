acacc4fef3e262a7a314fc202b3952d491358a52
Marcelino Budiman <marcelinobudiman47@gmail.com>
2022-06-10 14:57:21 +0700

    Set color to magenta for bar item

--- MC02-Group9/MC02-Group9/AddMedicationViewController.swift
+++ MC02-Group9/MC02-Group9/AddMedicationViewController.swift
@@ -20,9 +20,34 @@ class AddMedicationViewController: UIViewController {
     private func setNavItem(){
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        navigationController?.navigationBar.tintColor = hexStringToUIColor(hex: "#D33F9A")
+        
         navigationItem.title = "Tambah Jadwal Obat"
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Simpan", style: .plain, target: self, action: #selector(dismissSelf))
+    }
+    
+    // Function buat pake hex color
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
     
     @objc private func dismissSelf(){
