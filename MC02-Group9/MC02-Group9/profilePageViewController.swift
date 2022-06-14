3e75052630814b5ba5a573c2bb8286b6be9f19ae
Noah Syanahan <noahsyanahan@gmail.com>
2022-06-14 21:32:26 +0700

    profilePage functionality done

--- MC02-Group9/MC02-Group9/profilePageViewController.swift
+++ MC02-Group9/MC02-Group9/profilePageViewController.swift
@@ -8,14 +8,38 @@
 import UIKit
 
 class profilePageViewController: UIViewController {
-
+    
+    var dummyModel = [
+        "Metformin",
+        "Paracetamol",
+        "Aspirin",
+        "Milkita"
+    ]
+    
+    
+    @IBOutlet weak var obatKamuTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        obatKamuTableView.delegate = self
+        obatKamuTableView.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func editMedicationPressed(_ sender: Any) {
+        obatKamuTableView.isEditing = !obatKamuTableView.isEditing
+    }
+    
+//    @IBAction func btnClicked(_ sender: Any) {
+//        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "AddMedicationViewController") as? AddMedicationViewController else {
+//            print("Error")
+//            return
+//        }
+//
+//        let navVC = UINavigationController(rootViewController: vc)
+//
+//        present(navVC, animated: true)
+//
+//    }
     /*
     // MARK: - Navigation
 
@@ -26,4 +50,35 @@ class profilePageViewController: UIViewController {
     }
     */
 
+}
+
+
+
+extension profilePageViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = dummyModel[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            
+            //REMOVE FROM CORE DATA HERE!
+            dummyModel.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            tableView.endUpdates()
+        }
+    }
+    
 }
