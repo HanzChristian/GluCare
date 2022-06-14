df7ad06e918a522b463467d328bd9ac1951dd9db
Noah Syanahan <noahsyanahan@gmail.com>
2022-06-14 15:10:39 +0700

    add Profile Page to Tab Controller

---
+++ MC02-Group9/MC02-Group9/profilePageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  profilePageViewController.swift
+//  MC02-Group9
+//
+//  Created by Noah S on 14/06/22.
+//
+
+import UIKit
+
+class profilePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
