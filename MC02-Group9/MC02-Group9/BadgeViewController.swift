82cc7a82ef71023002d8e7d8b5c973676762dd71
Marcelino Budiman <marcelinobudiman47@gmail.com>
2022-06-21 18:25:43 +0700

    Refactor Badge Storyboard

---
+++ MC02-Group9/MC02-Group9/BadgeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BadgeViewController.swift
+//  MC02-Group9
+//
+//  Created by Marcelino Budiman on 21/06/22.
+//
+
+import UIKit
+
+class BadgeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Badge"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
