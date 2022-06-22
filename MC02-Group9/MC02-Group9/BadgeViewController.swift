acfda6b3b5e5914be0e7ec85385973228cc9de94
Marcelino Budiman <marcelinobudiman47@gmail.com>
2022-06-22 15:20:19 +0700

    Add progress bar and attributes to badge page

--- MC02-Group9/MC02-Group9/BadgeViewController.swift
+++ MC02-Group9/MC02-Group9/BadgeViewController.swift
@@ -8,24 +8,68 @@
 import UIKit
 
 class BadgeViewController: UIViewController {
-
+    
+//    private let lblStreak: UILabel = {
+//       let lblStreak = UILabel()
+//        lblStreak.textAlignment = .center
+//        lblStreak.text = """
+//                         1/7
+//                         day(s) streak
+//                         """
+//        lblStreak.font = .systemFont(ofSize: 12, weight: .light)
+//        return lblStreak
+//    }()
+    
+    @IBOutlet weak var lblDays: UILabel!
+    @IBOutlet weak var lblStreak: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Badge"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        // Do any additional setup after loading the view.
+        
+        configuration()
+        circleBar()
+        
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configuration(){
+        title = "Badge"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // config label
+        lblDays.text = "1/7"
+        lblStreak.text = "day streaks"
+//        lblStreak.sizeToFit()
+//        view.addSubview(lblStreak)
+//        lblStreak.center = view.center
     }
-    */
+    
+    func circleBar(){
+        let circlePath = UIBezierPath(arcCenter: view.center, radius: 150, startAngle: -(.pi/2), endAngle: .pi*2, clockwise: true)
+        
+        let trackShape = CAShapeLayer()
+        trackShape.path = circlePath.cgPath
+        trackShape.fillColor = UIColor.clear.cgColor
+        trackShape.lineWidth = 15
+        trackShape.strokeColor = UIColor.lightGray.cgColor
+        view.layer.addSublayer(trackShape)
+        
+        let shape = CAShapeLayer()
+        shape.path = circlePath.cgPath
+        shape.lineWidth = 15 // border
+        shape.fillColor = UIColor.clear.cgColor
+        shape.strokeEnd = 0
+        shape.strokeColor = UIColor.blue.cgColor
+        view.layer.addSublayer(shape)
+        
+        // animation
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.toValue = 0.4
+        animation.duration = 2
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = .forwards
+        shape.add(animation, forKey: "animation")
+    }
 
 }
