dd316c53f0a9fe4d972f2342990ffd5ec4f74927
RichardMulyadi <richardmulyadi3001@gmail.com>
2022-10-27 18:30:56 +0700

    profile-unfinished

---
+++ MC02-Group9/MC02-Group9/Controller/profileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  profileViewController.swift
+//  MC02-Group9
+//
+//  Created by Richard Mulyadi on 27/10/22.
+//
+
+import UIKit
+
+class profileViewController: UIViewController {
+    
+    @IBOutlet weak var profileTVC: UITableView!
+    @IBOutlet weak var userProfile: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MC02-Group9/MC02-Group9/View/ProfileCell/MedAdheranceTVC.swift
@@ -0,0 +1,25 @@
+//
+//  MedAdheranceTVC.swift
+//  MC02-Group9
+//
+//  Created by Richard Mulyadi on 27/10/22.
+//
+
+import UIKit
+
+class MedAdheranceTVC: UITableViewCell {
+    
+    @IBOutlet weak var adheranceTitleLbl: UILabel!
+    @IBOutlet weak var adheranceDataLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+}
