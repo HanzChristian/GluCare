d1239c283e14dcba4d08a6e4f36526c18482e829
RichardMulyadi <richardmulyadi3001@gmail.com>
2022-12-13 12:09:00 +0700

    statistic-stash

---
+++ MC02-Group9/MC02-Group9/Controller/Statistik/TVC/BGSTVC.swift
@@ -0,0 +1,27 @@
+//
+//  BGSTVC.swift
+//  MC02-Group9
+//
+//  Created by Richard Mulyadi on 09/12/22.
+//
+
+import UIKit
+
+class BGSTVC: UITableViewCell {
+
+    
+    @IBOutlet weak var percentageBGLbl: UILabel!
+    @IBOutlet weak var descBGLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
